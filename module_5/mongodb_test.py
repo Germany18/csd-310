d94ceb9a16c02075841dc4dc29b3edc2001aca85
Germany18 <88046155+Germany18@users.noreply.github.com>
2022-06-27 10:27:45 -0400

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -2,4 +2,4 @@ from pymongo import MongoClient
 url = "mongodb+srv://Bekkah93:Severus1993.@cluster0.vygwbkg.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient (url)
 db = client.pytech
-print(db.list_collection_names)
+print(db.list_collection_names())
